631a86fc92966734e23b5a117182e3fcb737d819
2qp <56475797+2qp@users.noreply.github.com>
2025-02-02 22:39:19 +0530

    feat(shadcn-ui-extended): add `RHFRadio`

---
+++ packages/shadcn-ui-extended/src/components/rhf/rhf-radio.tsx
@@ -0,0 +1,108 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@repo/shadcn-ui/components/form";
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from "@repo/shadcn-ui/components/radio-group";
+import { cn } from "@repo/shadcn-ui/lib/utils";
+
+import type { ComponentPropsWithoutRef, JSX, ReactNode } from "react";
+import type { Control, FieldValues, Path } from "react-hook-form";
+
+type RadioGroupItemProps = ComponentPropsWithoutRef<typeof RadioGroupItem>;
+
+// see .vscode tw attributes
+type RHFRadioStyles = {
+  groupClassName?: string;
+  labelClassName?: string;
+  descriptionClassName?: string;
+};
+
+type RHFRadioDataPoint = {
+  label: ReactNode;
+  leading?: ReactNode;
+} & RadioGroupItemProps;
+
+type RHFRadioProps<T extends FieldValues> = {
+  name: Path<T>;
+  control: Control<T>;
+
+  data: RHFRadioDataPoint[] | Readonly<RHFRadioDataPoint[]>;
+
+  readOnly?: boolean;
+  disabled?: boolean;
+
+  label?: string;
+  description?: string;
+
+  styles?: RHFRadioStyles;
+};
+
+type RHFRadioType = <T extends FieldValues>(
+  props: RHFRadioProps<T>
+) => JSX.Element;
+
+const RHFRadio: RHFRadioType = ({
+  control,
+  name,
+  description,
+  label,
+  styles,
+  data,
+  disabled = false,
+  readOnly = false,
+}) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="space-y-3">
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            {/*  */}
+            <RadioGroup
+              defaultValue={field.value}
+              onValueChange={field.onChange}
+              className={cn("flex flex-col space-y-1", styles?.groupClassName)}
+              disabled={disabled}
+              aria-readonly={readOnly}
+              {...field}
+            >
+              {data?.map((point, index) => {
+                //
+                return (
+                  <FormItem
+                    key={index}
+                    className="flex items-center space-x-3 space-y-0"
+                  >
+                    <FormControl>
+                      <RadioGroupItem {...point} />
+                    </FormControl>
+                    <FormLabel>{point.label}</FormLabel>
+                  </FormItem>
+                );
+              })}
+
+              {/*  */}
+            </RadioGroup>
+            {/*  */}
+          </FormControl>
+          <FormDescription className={cn(styles?.descriptionClassName)}>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export { RHFRadio };
+export type { RHFRadioDataPoint, RHFRadioProps, RHFRadioType };
