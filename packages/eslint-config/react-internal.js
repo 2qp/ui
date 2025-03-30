f419e6d7dd6ab92102f24ab67d4da1a48767de2f
2qp <56475797+2qp@users.noreply.github.com>
2025-03-30 22:51:42 +0530

    chore(eslint-config/deps): bump `devDeps` versions

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -9,7 +9,7 @@ import { config as baseConfig } from "./base.js";
 /**
  * A custom ESLint configuration for libraries that use React.
  *
- * @type {import("eslint").Linter.Config} */
+ * @type {import("eslint").Linter.Config[]} */
 export const config = [
   ...baseConfig,
   js.configs.recommended,
@@ -34,7 +34,6 @@ export const config = [
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
     },
   },
 ];
