b37a2c7ab3d336b2c55e903c17395596bba98fe4
2qp <56475797+2qp@users.noreply.github.com>
2025-09-13 12:42:33 +0530

    feat(shadcn-ui-extended): add `RHFMultiComboBox` | `use-multi-combo`

---
+++ packages/shadcn-ui-extended/src/components/rhf/rhf-multi-combo-box.tsx
@@ -0,0 +1,202 @@
+import { useMultiCombo } from "@repo/shadcn-ui-extended/hooks/use-multi-combo";
+import { Button } from "@repo/shadcn-ui/components/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@repo/shadcn-ui/components/command";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@repo/shadcn-ui/components/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@repo/shadcn-ui/components/popover";
+import { cn } from "@repo/shadcn-ui/lib/utils";
+import clsx from "clsx";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import type { DataLifeCycle } from "@repo/shadcn-ui-extended/hooks/use-multi-combo";
+import type { JSX } from "react";
+import type { Control, FieldValues, Path } from "react-hook-form";
+
+// see .vscode tw attributes
+type RHFMultiComboBoxStyles = {
+  comboClassName?: string;
+  comboItemClassName?: string;
+  labelClassName?: string;
+  descriptionClassName?: string;
+};
+
+// --- TYPE GUARD ---
+
+type StringLiteralArrayKeys<T> = {
+  [K in keyof T]: T[K] extends `${infer _}`[] ? K : never;
+}[keyof T];
+
+type StringArrayKeys<T> = {
+  [K in keyof T]: T[K] extends string[] ? K : never;
+}[keyof T];
+
+type RestrictKeys<
+  T extends string | string[],
+  TShape,
+  TSafety extends boolean = true,
+> = TSafety extends true
+  ? T extends StringLiteralArrayKeys<TShape>
+    ? T
+    : never
+  : T extends StringArrayKeys<TShape>
+    ? T
+    : never;
+// --- ---
+
+type RHFMultiComboBoxProps<
+  T extends FieldValues,
+  TSafety extends boolean = true,
+> = {
+  name: RestrictKeys<Path<T>, T, TSafety>;
+  control: Control<T>;
+
+  safety?: TSafety;
+
+  readOnly?: boolean;
+  disabled?: boolean;
+
+  label?: string;
+  empty?: string;
+  placeholder?: string;
+  searchholder?: string;
+  description?: string;
+
+  styles?: RHFMultiComboBoxStyles;
+} & DataLifeCycle;
+
+type RHFMultiComboBoxType = <
+  T extends FieldValues,
+  TSafety extends boolean = true,
+>(
+  props: RHFMultiComboBoxProps<T, TSafety>
+) => JSX.Element;
+
+const RHFMultiComboBox: RHFMultiComboBoxType = ({
+  control,
+  name,
+  label,
+  empty,
+  placeholder,
+  searchholder,
+  description,
+  styles,
+  safety,
+  readOnly,
+  disabled,
+  ...payload
+}) => {
+  //
+
+  const [getSelectedLabels, handleSelect, isSelected] = useMultiCombo(payload);
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel className={cn(styles?.labelClassName)}>{label}</FormLabel>
+          <Popover modal={true}>
+            <PopoverTrigger ref={field.ref} asChild>
+              <FormControl>
+                <Button
+                  disabled={disabled}
+                  variant="outline"
+                  role="combobox"
+                  className={cn(
+                    "w-[200px] justify-between",
+                    !field.value && "text-muted-foreground"
+                  )}
+                >
+                  <p className="overflow-hidden truncate">
+                    {getSelectedLabels(field.value).join(", ") || placeholder}
+                  </p>
+
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput
+                  readOnly={readOnly}
+                  placeholder={searchholder}
+                  className="h-9"
+                />
+                <CommandList>
+                  <CommandEmpty>{empty}</CommandEmpty>
+
+                  {payload.data.map((group) => (
+                    <CommandGroup key={group.id} heading={group.label}>
+                      {group.items.map((item, index) => {
+                        const selected = isSelected(item.value, field.value);
+
+                        return (
+                          <CommandItem
+                            {...item}
+                            value={item.value}
+                            keywords={[item.label]}
+                            key={item.id || index}
+                            onSelect={() => {
+                              const result = handleSelect(
+                                item.value,
+                                field.value
+                              );
+
+                              field.onChange(result);
+                            }}
+                          >
+                            <div className="flex items-center justify-between w-full">
+                              <div>{item.leading}</div>
+                              <div>{item.label}</div>
+                              <Check
+                                className={clsx(
+                                  "ml-auto transition-opacity duration-75",
+                                  {
+                                    "opacity-0": !selected,
+                                    "opacity-100": selected,
+                                  }
+                                )}
+                              />
+                            </div>
+                          </CommandItem>
+                        );
+                      })}
+                    </CommandGroup>
+                  ))}
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+          <FormDescription className={cn(" ", styles?.descriptionClassName)}>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+        /* */
+      )}
+    />
+
+    /* */
+  );
+};
+
+export { RHFMultiComboBox };
+export type { RHFMultiComboBoxProps, RHFMultiComboBoxType };
---
+++ packages/shadcn-ui-extended/src/hooks/use-multi-combo.ts
@@ -0,0 +1,184 @@
+import { useCallback, useMemo } from "react";
+
+import type { ReactNode } from "react";
+import type { FieldError } from "react-hook-form";
+
+type ComboBoxItem = {
+  id: string;
+  label: string;
+  value: string;
+  groupId?: string;
+  isSelectable?: boolean;
+  keywords?: string[];
+  leading?: ReactNode;
+};
+// & RHFCommandItemProps;
+
+type ComboBoxGroup = {
+  id: string;
+  label?: string;
+  items: ComboBoxItem[];
+};
+
+type Indexes = {
+  labelsByValue: Record<string, string>;
+};
+
+type Intake = {} & Indexes;
+
+type DataLifeCycle = { data: ComboBoxGroup[] | Readonly<ComboBoxGroup[]> } & (
+  | {
+      precompute: true;
+    }
+  | {
+      indexes: Intake;
+      precompute: false;
+    }
+);
+
+// type UseMultiComboBoxParams = {  };
+
+type UseMultiComboReturnType = [
+  (selectedValues: string[]) => string[],
+  (value: string, selectedValues: string[]) => string[],
+  (value: string, selectedValues: string[]) => boolean,
+  (error: FieldError | undefined) => string,
+  Indexes["labelsByValue"],
+];
+
+type UseMultiComboType = (params: DataLifeCycle) => UseMultiComboReturnType;
+
+const useMultiCombo: UseMultiComboType = (payload) => {
+  const indexes = useMemo(() => {
+    //
+    if (!payload.precompute) return payload.indexes;
+
+    //
+    const labelsByValue: Record<string, string> = {};
+    const groups = payload.data;
+    for (let i = 0; i < groups.length; i++) {
+      const group = groups[i];
+      if (!group) continue;
+
+      for (let j = 0; j < group.items.length; j++) {
+        const item = group.items[j];
+        if (!item) continue;
+
+        labelsByValue[item.value] = item.label;
+      }
+    }
+
+    return { labelsByValue };
+  }, [payload]);
+
+  //
+  const getSelectedLabels = useCallback(
+    (selectedValues: string[]): string[] => {
+      if (!Array.isArray(selectedValues) || !selectedValues.length) return [];
+
+      return selectedValues
+        .map((val) => indexes.labelsByValue?.[val])
+        .filter((label) => label !== undefined) as string[];
+    },
+    [indexes.labelsByValue]
+  );
+
+  //
+  const handleSelect = useCallback(
+    (value: string, selectedValues: string[]) => {
+      //
+      const set = new Set<string>(selectedValues || []);
+
+      if (set.has(value)) {
+        //
+        set.delete(value);
+        return [...set];
+      }
+
+      set.add(value);
+      return [...set];
+    },
+    []
+  );
+
+  //
+  const isSelected = useCallback((value: string, selectedValues: string[]) => {
+    const set = new Set(selectedValues);
+
+    return set.has(value);
+  }, []);
+
+  //
+  const getErrorMessages = (error: FieldError | undefined) => {
+    if (!error) return "";
+    if (!Array.isArray(error)) return "";
+
+    return Object.values((error as FieldError[]) || {})
+      .map((err) => err?.message)
+      .filter(Boolean)
+      .join(", ");
+  };
+
+  // const groupsx: ComboBoxGroup[] = useMemo(() => {
+  //   if (!data.groups?.length) {
+  //     //
+  //     if (!precompute) {
+  //       return [
+  //         { id: "n_parents", label: undefined, itemIds: data.flatIds || [] },
+  //       ];
+  //     }
+
+  //     // precompute [true]
+  //     const length = data.items.length;
+  //     const itemIds = new Array(length);
+  //     for (let i = 0; i < length; i++) {
+  //       itemIds[i] = data.items[i]?.id as string;
+  //     }
+
+  //     return [{ id: "n_parents", label: undefined, itemIds }];
+  //   }
+
+  //   return data.groups;
+  // }, [data, precompute]);
+
+  // const errorMessage = useMemo(() => {
+  //   if (!controller.fieldState.error) return "";
+  //   if (!Array.isArray(controller.fieldState.error)) return "";
+
+  //   return Object.values((controller.fieldState.error as FieldError[]) || {})
+  //     .map((err) => err?.message)
+  //     .filter(Boolean)
+  //     .join(", ");
+  // }, [controller.fieldState.error]);
+
+  return [
+    getSelectedLabels,
+    handleSelect,
+    isSelected,
+    getErrorMessages,
+    indexes.labelsByValue,
+  ];
+};
+
+type RHFMultiComboBoxItem = ComboBoxItem;
+type RHFMultiComboBoxGroup = ComboBoxGroup;
+
+export { useMultiCombo };
+export type {
+  DataLifeCycle,
+  RHFMultiComboBoxGroup,
+  RHFMultiComboBoxItem,
+  UseMultiComboType,
+};
+
+type ComboGroup = {
+  id: string;
+  label: string;
+  items: ComboItem[];
+};
+
+type ComboItem = {
+  id: string;
+  label: string;
+  value: string;
+};
