dcb2d0b2cab67796e49ea7256bfdff71ab834566
2qp <56475797+2qp@users.noreply.github.com>
2025-10-04 23:33:18 +0530

    chore(shadcn-ui-extended): move `prioritized-phone-codes` into `/lib/`

--- packages/shadcn-ui-extended/src/hooks/use-phone.ts
+++ packages/shadcn-ui-extended/src/hooks/use-phone.ts
@@ -5,7 +5,7 @@ import parsePhoneNumberFromString, {
 } from "libphonenumber-js";
 import { useCallback, useMemo } from "react";
 
-import { PRIORITIZED_PHONE_CODES } from "@/constants/prioritized-phone-codes";
+import { PRIORITIZED_PHONE_CODES } from "@/lib/prioritized-phone-codes";
 
 import type { CountryCode } from "libphonenumber-js";
 
